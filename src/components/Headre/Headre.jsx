b346a4638e517a098beb8e951c9d05039b17c2ec
adiletbectursunov <adibektursunov@gmail.com>
2023-04-05 02:02:46 +0600

    practika

---
+++ src/components/Headre/Headre.jsx
@@ -0,0 +1,14 @@
+import classes from './Headre.module.css'
+
+export const Headre = () => {
+  return(
+    <div className={classes.headre}>
+      privet
+      <div>
+         тепение и труд никогда не предадут ф вот мечта предать может.
+      </div>
+    </div>
+  )
+}
+
+export default Headre
---
+++ src/components/section/Section.jsx
@@ -0,0 +1,16 @@
+import classes from './Section.module.css'
+
+export const Section = () => {
+  return(
+    <div className={classes.headre}>
+      privet
+      <div>
+         тепение и труд никогда не предадут а вот мечта предать может
+         ествеенно что все мы стараемсчя исполнить свою мечту <br />
+         но чаще всего оно ведь не исполняется но терпени труда для нас станут утиением
+      </div>
+    </div>
+  )
+}
+
+export default Section
